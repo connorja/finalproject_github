f98b8b5c1c5704e15b11d3795c4f9fabc0d2184f
Jacob Connor <jacob.connor@bmwfs.com>
2022-12-21 11:57:28 -0500

    Edit to line 196 so code runs

--- bikeshare.py
+++ bikeshare.py
@@ -193,7 +193,7 @@ def main():
         row_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        while restart != 'yes' or restart != 'no':
+        while restart != 'yes' and restart != 'no':
             restart = input('\nPlease enter yes or no.\n')
         if restart.lower() != 'yes':
             break
